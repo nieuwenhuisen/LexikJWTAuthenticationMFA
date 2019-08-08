bfd54ce302d00f49f00bdd4c1bb07957bc27d566
Jeroen van den Nieuwenhuisen <nieuwenhuisen@me.com>
2019-08-08 08:16:48 +0200

    Make MFA nullable and add isMFaEnabled to trait.

--- src/Traits/UseGoogleAuthenticatorTrait.php
+++ src/Traits/UseGoogleAuthenticatorTrait.php
@@ -7,20 +7,25 @@ use Doctrine\ORM\Mapping as ORM;
 trait UseGoogleAuthenticatorTrait
 {
     /**
-     * @var string
+     * @var string|null
      * @ORM\Column(type="string", length=180, nullable=true)
      */
     private $mfaKey;
 
-    public function getMfaKey(): string
+    public function getMfaKey(): ?string
     {
         return $this->mfaKey;
     }
 
-    public function setMfaKey(string $mfaKey): self
+    public function setMfaKey(?string $mfaKey): self
     {
         $this->mfaKey = $mfaKey;
 
         return $this;
+    }
+
+    public function isMfaEnabled(): bool
+    {
+        return null !== $this->mfaKey;
     }
 }
